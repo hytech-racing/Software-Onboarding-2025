464e6a94d9ed1bfdb171e44fccea763ec982cc1d
sreekara <sreedansd@gmail.com>
2024-08-25 18:46:15 -0400

    added medium mode template solution

---
+++ Sreekara/Part-1/src/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hello.hpp"
+
+#include <iostream>
+#include "hello.hpp"
+
+using namespace std;
+
+void Hello::hello_world() {
+    cout << "Hello, world!" << endl;
+}
