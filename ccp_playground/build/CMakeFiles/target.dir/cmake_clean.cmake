fb09b0d415af11a6cdc1963dc1a84574944b501e
Christine <132790780+aesteri@users.noreply.github.com>
2024-08-29 08:08:08 -0500

    Lab 2

---
+++ ccp_playground/build/CMakeFiles/target.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/target.dir/app/playing.cpp.o"
+  "CMakeFiles/target.dir/app/playing.cpp.o.d"
+  "target"
+  "target.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/target.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ccp_playground/build/CMakeFiles/target.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for target.
+# This may be replaced when dependencies are built.
