5defc3a2cddd319f7b3f83067b990c4d9509cf50
RenchTG <dobranowskip1@gmail.com>
2024-08-29 17:07:27 -0400

    lab 2, part 1

---
+++ PhilipDobranowski/lab2_part1/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Hello World part 1 flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    easy_cmake.url = "github:RCMast3r/easy_cmake";
+  };
+
+  outputs = { self, nixpkgs, easy_cmake }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+      };
+      hello_world_lib_pkg = pkgs.callPackage ./default.nix { };
+    in
+    {
+      packages.x86_64-linux = rec {
+        hello_world_lib = hello_world_lib_pkg;
+        default = hello_world_lib;
+      };
+    };
+}
