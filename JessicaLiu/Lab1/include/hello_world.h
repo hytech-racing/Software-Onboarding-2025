fe2323f10b0ca7ef2f624b78bc882b30fcc78e38
jessicaliu06 <jessicatracyliu@gmail.com>
2024-08-29 16:27:46 -0400

    Lab 1: Part1 & Part 1

---
+++ JessicaLiu/Lab1/include/hello_world.h
@@ -0,0 +1,9 @@
+#ifndef _HELLO_WORLD_H_
+#define _HELLO_WORLD_H_
+
+class hello_world {
+    public:
+        void helloWorld();
+};
+
+#endif
---
+++ JessicaLiu/Lab1/src/hello_world.cpp
@@ -0,0 +1,7 @@
+#include "\\wsl.localhost\Ubuntu\home\jessicaliu\Lab1\include\hello_world.h"
+
+#include<iostream>
+
+void hello_world::helloWorld() {
+    std::cout << "Hello, World!\n";
+}
