234ed3f9a5c3f1552ce396c521ffee4e1649b4b7
Mitchell Comer <mitchcomer@icloud.com>
2024-08-29 11:28:33 -0400

    lab 1 hopefully completed

---
+++ mitchellcomerlab1/lab1_hard_project/include/hello_world.h
@@ -0,0 +1,4 @@
+class hello_world{
+public:
+    void print_hello_world();
+};
---
+++ mitchellcomerlab1/lab1_hard_project/src/hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "hello_world.h"
+
+void hello_world::print_hello_world(){
+    std::cout << "Hello, World from the hellolib!\n";
+}
