27f5b7412fb645786c0bf71e9d89318f2836de05
Марк Гольцман <markgolcman@lawn-143-215-99-90.lawn.gatech.edu>
2024-08-29 01:33:20 -0400

    Completed lab1

---
+++ goltsmannn2/src/hello_world.h
@@ -0,0 +1,13 @@
+#ifndef HELLO_WORLD_H
+#define HELLO_WORLD_H
+#include <string>
+
+class HelloWorld{
+public:
+    std::string hello_world(){
+        return "Hello, world!";
+    }
+};
+
+
+#endif //HELLO_WORLD_H
