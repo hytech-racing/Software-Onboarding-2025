59c0315d34442d2d7a988074660c520de65263b7
Cade Wooten <cadewoot@outlook.com>
2024-09-03 19:14:41 -0400

    Completed Lab1 (Library Version)

---
+++ ECW_OnBoarding_Labs/Lab1/src/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+#include <print.h>
+using namespace std;
+
+int main(){
+    int a, b;
+    cout << "Hello World! Please input two integers:\n";
+    cin >> a;
+    cin >> b;
+    cout << add(a,b);
+    cin.get();   
+}
---
+++ ECW_OnBoarding_Labs/Lab1/src/print.cpp
@@ -0,0 +1,5 @@
+#include "print.h"
+
+int add(int a, int b) {
+    return (a+b);
+}
