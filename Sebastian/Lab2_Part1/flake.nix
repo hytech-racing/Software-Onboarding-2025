a0f5b74c0f035eb477e4ff472dfa4f4fbdfab137
Sebastian <sebastianveras06@hotmail.com>
2024-09-10 13:18:05 -0400

    Saving

--- Sebastian/Lab2_Part1/flake.nix
+++ Sebastian/Lab2_Part1/flake.nix
@@ -1,23 +1,25 @@
 {
-    description = "Lab 2 Part 1 Flake"
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    };
-    outputs = {self, nixpkgs}:
-    let
-        hello_lib_overlay = final: prev: {
-            hello_lib = final.callPackage ./default.nix { };
-            #This line defines hello_lib as the derivation from default.nix
-            #Important stuff
-        };
-        my_overlays = [hello_lib_overlay];
+  description = "part 1 flake";
 
-        pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            overlays = [self.overlays.default];
-        };
-    in {
-        pachages.x86_64-linux.default = pkgs.hello_lib;
-        overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    hello_lib_overlay = final: prev: {
+      hello_lib = final.callPackage ./default.nix { };
+    };
+    my_overlays = [ hello_lib_overlay ];
+
+
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = [ self.overlays.default ];
+    };  
+  in
+  {      
+    packages.x86_64-linux.default = pkgs.hello_lib;
+    overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+  };
 }
