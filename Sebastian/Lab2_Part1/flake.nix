8da6429e160621859a0fddccc39bb57341315daf
Sebastian <sebastianveras06@hotmail.com>
2024-09-10 13:25:11 -0400

    Lab 2 Part 1

--- Sebastian/Lab2_Part1/flake.nix
+++ Sebastian/Lab2_Part1/flake.nix
@@ -1,25 +1,30 @@
 {
-  description = "part 1 flake";
+  description = "part 1 flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
-  let
-    hello_lib_overlay = final: prev: {
-      hello_lib = final.callPackage ./default.nix { };
-    };
-    my_overlays = [ hello_lib_overlay ];
+  outputs = { self, nixpkgs }:
+  let
+    hello_lib_overlay = final: prev: {
+      hello_lib = final.callPackage ./default.nix { };
+    };
 
+    # Define overlays as a list
+    my_overlays = [ hello_lib_overlay ];
 
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      overlays = [ self.overlays.default ];
-    };  
-  in
-  {      
-    packages.x86_64-linux.default = pkgs.hello_lib;
-    overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
-  };
+    # Import nixpkgs with overlays applied
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      overlays = my_overlays;
+    };  
+  in
+  {      
+    # Define the default package for x86_64-linux
+    packages.x86_64-linux.default = pkgs.hello_lib;
+
+    # Expose the overlays for reuse if necessary
+    overlays.default = hello_lib_overlay;
+  };
 }
