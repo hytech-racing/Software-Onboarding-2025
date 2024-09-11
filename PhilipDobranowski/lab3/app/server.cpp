ceb93a941d87fdb533de1eee805c33414a13dd3b
Philip Dobranowski <dobranowskip1@gmail.com>
2024-09-11 14:20:13 -0400

    lab 3 squash (#66)
    * actually lab 3
    
    * Final changes
    
    * Delete result!

---
+++ PhilipDobranowski/lab3/app/server.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Comm.hpp>
+#include <message.pb.h>
+
+int main()
+{
+    // Comm comm("192.168.203.1", 13337);
+    Comm comm("127.0.0.1", 13337);
+    comm.bind();
+    
+    lab3Proto::SimpleMessage msg;
+    
+    while (true) {
+        std::string buf = comm.recv();
+        msg.ParseFromString(buf);
+        std::cout << msg.sender_name() << " sent: " << msg.message() << std::endl;
+
+        msg.set_sender_name("Server");
+        msg.set_message("Server received your message of \"" + msg.message() + "\"!");
+        std::string serialized_message;
+        msg.SerializeToString(&serialized_message);
+        comm.send(serialized_message, 1);
+    }
+}
