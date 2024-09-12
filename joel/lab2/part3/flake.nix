cbf8b93c1ebf77eecb29ac0002175e1f89d6d2ca
jjoel1630 <jjoel1630@gmail.com>
2024-09-12 19:04:32 -0400

    finished lab 2 completely

--- joel/lab2/part3/flake.nix
+++ joel/lab2/part3/flake.nix
@@ -1,47 +1,46 @@
 {
-  description = "fsae onboarding c++ library";
-
+  description = "lab2 part 3";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     utils.url = "github:numtide/flake-utils";
+    hellolib.url = "github:jjoel1630/hytech-fsae-lab2.3";
   };
-  outputs = { self, nixpkgs, utils }:
+  
+  outputs = { self, nixpkgs, utils, hellolib }:
     let
-      target_overlay = final: prev: {
-        target = final.callPackage ./default.nix { };
+      target_overlay = final: prev: rec { 
+        target = final.callPackage ./default.nix { 
+        };
       };
-      my_overlays = [ target_overlay ];
+
+      my_overlays = [ target_overlay hellolib.overlays.default ];
+
       pkgs = import nixpkgs {
         system = "aarch64-darwin";
+        # system = "x86_64-darwin";
         overlays = [ self.overlays.default ];
       };
     in
     {
       overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
-
-      packages.aarch64-darwin =
-        rec {
-          target = pkgs.target;
-          default = target;
-        };
+      packages.aarch64-darwin.default = pkgs.target;
+      # packages.x86_64-darwin.default = pkgs.target;
 
       devShells.aarch64-darwin.default =
+      # devShells.x86_64-darwin.default =
         pkgs.mkShell rec {
-          # Update the name to something that suites your project.
-          name = "nix-devshell";
+          name = "joel's playground";
           packages = with pkgs; [
-            # Development Tools
             cmake
             target
           ];
 
-          # Setting up the environment variables you need during
-          # development.
           shellHook =
             let
               icon = "f121";
             in
             ''
+              echo "hello world"
               export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
             '';
         };
