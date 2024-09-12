1a952ee14aaf066417246b6534f76c493826dd77
Miles Kent <mileskent13@gmail.com>
2024-09-12 19:54:39 -0400

    socket WIP

--- milesK/milesKlab3/src/socket.cpp
+++ milesK/milesKlab3/src/socket.cpp
@@ -1,41 +1,77 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+#include <arpa/inet.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
 
-#define BUFSIZE 2048
-#define PORT 1153
+#define BUFFERSIZE 2048
 
-int main() {
-    struct sockaddr_in myaddr;      /* our address */
-    struct sockaddr_in remaddr;     /* remote address */
-    socklen_t addrlen = sizeof(remaddr);            /* length of addresses */
-    int recvlen;                    /* # bytes received */
-    int fd;                         /* our socket */
-    unsigned char buf[BUFSIZE];     /* receive buffer */
+class Socket {
+    int port;
+    int fd;
 
-    /* create a UDP socket */
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-            perror("cannot create socket\n");
+public:
+    Socket (int port) {
+        this->port = port;
+        this->fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+        if (fd < 0) {
+            std::cerr << "Failed to create socket\n";
             return 0;
+        }
+
+        struct sockaddr_in server_addr;
+        memset(&server_addr, 0, sizeof(server_addr));
+        server_addr.sin_family = AF_INET;
+        server_addr.sin_addr.s_addr = INADDR_ANY;
+        server_addr.sin_port = htons(port);
+
+        if (bind(fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+            std::cerr << "Failed to bind socket\n";
+            return 0;
+        }
+    }
+    ~Socket() {
+        close(fd);
     }
 
-    memset((char *)&myaddr, 0, sizeof(myaddr));
-    myaddr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    myaddr.sin_port = htons(PORT);
+    void sendMsg (char* message, char* remoteIP, int port) const {
+		struct sockaddr_in remoteAddr;
+        // Populate remoteAddr
+		memset((char*)&remoteAddr, 0, sizeof(recipientAddr));
+		remoteAddr.sin_family = AF_INET;
+		remoteAddr.sin_port = htons(port);
+		inet_pton(AF_INET, remoteIP, &remoteAddr.sin_addr);
 
-    if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        // Send msg
+		if (sendto(this->fd, message, strlen(message), 0, (struct sockaddr*)&remoteAddr, sizeof(recipientAddr)) < 0) {
+			std::cerr << "sendto failed" << std::endl;
+			return 0;
+		}
+    }
+
+    char* getMsg (char* remoteIP, int port) const {
+        struct sockaddr_in localAddr;
+        struct sockaddr_in remoteAddr;
+        socklen_t addrlen = sizeof(remoteAddr);
+        unsigned char buf[BUFFERSIZE] = "";
+
+
+        // bind the socket to any valid IP address and a specific port
+        memset((char*)&localAddr, 0, sizeof(localAddr));
+        localAddr.sin_family = AF_INET;
+        localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        localAddr.sin_port = htons(port);
+
+        if (bind(this->fd, (struct sockaddr*)&localAddr, sizeof(localAddr)) < 0 {
             perror("bind failed");
             return 0;
-    }
+        }
 
-    for (;;) {
-            printf("waiting on port %d\n", PORT);
-            recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
-            printf("received %d bytes\n", recvlen);
-            if (recvlen > 0) {
-                    buf[recvlen] = 0;
-                    printf("received message: \"%s\"\n", buf);
-            }
+        // Get the msg 
+        if (recvfrom(this->fd, buf, BUFFERSIZE, 0, (struct sockaddr*)&remoteAddr, &addrlen) > 0) buf[recvlen] = '\0';
+        return buf;
     }
 }
---
+++ milesK/milesKlab3/src/test.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define BUFSIZE 2048
+
+int main() {
+    struct sockaddr_in myaddr;      /* our address */
+    struct sockaddr_in remaddr;     /* remote address */
+    socklen_t addrlen = sizeof(remaddr);            /* length of addresses */
+    int recvlen;                    /* # bytes received */
+    int fd;                         /* our socket */
+    unsigned char buf[BUFSIZE];     /* receive buffer */
+    int PORT = 0;
+
+    /* create a UDP socket */
+    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            perror("cannot create socket\n");
+            return 0;
+    }
+
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    myaddr.sin_port = htons(PORT);
+
+    if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+            perror("bind failed");
+            return 0;
+    }
+
+    for (;;) {
+            printf("waiting on port %d\n", PORT);
+            recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
+            printf("received %d bytes\n", recvlen);
+            if (recvlen > 0) {
+                    buf[recvlen] = 0;
+                    printf("received message: \"%s\"\n", buf);
+            }
+    }
+}
