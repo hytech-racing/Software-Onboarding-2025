6b9360d3b9ffe8b8ff2d8aee6eb0d340103522b7
dnguyen-hub <duy.nguyen.2005.college@gmail.com>
2024-09-13 17:33:00 -0400

    Revert "part 3 flake practice git"
    This reverts commit 0b1ee7f090b065865907e82373eb4c6a28be4815.
    
    <accidental commit>

--- DerekNgu/lab2/part3/default.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, cmake, hellolib }:
-
-stdenv.mkDerivation rec {
-  pname = "hello_lib";
-  version = "0.1.0";
-  src = ./.;
-  nativeBuildInputs = [ cmake ];
-  buildInputs = [ hellolib ];
-}
--- DerekNgu/lab2/part3/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <hello_world.hpp>
-
-int main()
-{
-    hello display;
-    display.print_hello();
-
-    return 0;
-}
