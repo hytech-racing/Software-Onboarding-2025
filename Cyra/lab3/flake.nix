58086093601415f9254b6a96f7ae6672302e0e1d
Cyra Alesha <cyraanindyaalesha@gmail.com>
2024-09-12 20:07:42 -0400

    idk whats up w the flake

--- Cyra/lab3/flake.nix
+++ Cyra/lab3/flake.nix
@@ -1,41 +1,35 @@
 {
   description = "flake lab 3";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "aarch64-darwin";
-    pkgs = import nixpkgs {
-      overlays = [
-        (final: _: { lab3 = final.callPackage ./default.nix { }; })
-      ];
+    let
+      system = "aarch64-darwin";
+
+      main_overlay = final: prev: rec {
+        main = final.callPackage ./default.nix { };
+      };
+
+      my_overlays = [ main_overlay ];
+
+      pkgs = import nixpkgs {
+        system = "aarch64-darwin";
+        overlays = [ self.overlays.default ];
+      };
+    in
+    {
+      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+      packages.aarch64-darwin.default = pkgs.main;
+      
+      legacyPackages.aarch64-darwin =
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: _: { lab3 = final.callPackage ./default.nix { }; })
+        ];
+      };
     };
-  in rec {
-      packages.${system}.default = pkgs.lab3;
-      # defaultPackage.${system} = legacyPackages.mkShell {
-      # nativeBuildInputs = [ legacyPackages.cmake ];
-      # buildInputs = [ legacyPackages.protobuf legacyPackages.gcc ];
-    };
-
-    devShells.aarch64-darwin.default = pkgs.mkShell rec {
-      name = "nix-devshell";
-      packages = with pkgs; [
-        cmake
-        hello_app
-      ];
-
-      shellHook = let
-        icon = "f121";
-      in
-      ''
-        export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-      '';
-    }; 
-
-    # legacyPackages.${system} = legacyPackages;
-  
-
 }
