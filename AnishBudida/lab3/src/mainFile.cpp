6b623a965605510307ab14f6f441cec470087129
anishthebud <anishbudi@gmail.com>
2024-09-15 17:13:01 -0400

    made serialized a string

--- AnishBudida/lab3/src/mainFile.cpp
+++ AnishBudida/lab3/src/mainFile.cpp
@@ -29,7 +29,7 @@ class mainServer {\
     };
 
     bool send(const std::String& message, u_16int port) {
-        int serialized;
+        std::string serialized;
         if (!message.serializeToString(&serialized)) {
             std::cout << "Message could not be serialized" << std::endl;
             return false;
