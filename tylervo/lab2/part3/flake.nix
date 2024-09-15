2b1fd41266e73c1ce2054a573659f1eb47017bd8
Tyler Vo <tvo@Tylers-MacBook-Pro-2.local>
2024-09-15 17:01:36 -0400

    part3 files

---
+++ tylervo/lab2/part3/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "tyler's flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hellolib.url = "github:tylerdvo/hellolib";
+  };
+
+  outputs = { self, nixpkgs, hellolib }:
+    let
+      hello_overlay = final: prev: rec {
+        hello = final.callPackage ./default.nix { };
+      };
+
+      my_overlays = [ hello_overlay ];
+
+      pkgs = import nixpkgs {
+        system = "aarch64-darwin";
+        overlays = [ self.overlays.default hellolib.overlays.default ];
+      };
+    in
+    {
+      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+      packages.aarch64-darwin.default = pkgs.hello;
+    };
+}
