602481dfe1d129e08bae9cc94cd4241887f13309
Mark Goltsman <mgoltsman3@gatech.edu>
2024-09-17 21:36:58 -0400

    Implemented server communication w/ protobuf

--- goltsmannn2/part3/client.cpp
+++ goltsmannn2/part3/client.cpp
@@ -6,17 +6,30 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <protobuf.pb.h>
 
 #define PORT 8000
+
+
 int main(){
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    const int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(PORT);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
+
     connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    const char* message = "Hello, server!";
-    send(clientSocket, message, strlen(message), 0);
+    OnboardingLab3::SimpleMessage message;
+    std::string name, msg;
+    std::cin >> name >> msg;
+    message.set_sender_name(name);
+    message.set_sender_message(msg);
+    std::string test;
+    message.SerializeToString(&test);
+    send(clientSocket, test.c_str(), test.size(), 0);
     close(clientSocket);
+
+    return 0;
 }
 
--- goltsmannn2/part3/server.cpp
+++ goltsmannn2/part3/server.cpp
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <protobuf.pb.h>
 
 #define PORT 8000
 
@@ -30,9 +31,11 @@ int main()
 
     // recieving data
     char buffer[1024] = { 0 };
-    recv(clientSocket, buffer, sizeof(buffer), 0);
-    std::cout << "Message from client: " << buffer
-              << std::endl;
+    auto len = recv(clientSocket, buffer, sizeof(buffer), 0);
+    std::cout << len << std::endl;
+    OnboardingLab3::SimpleMessage receivedMessage;
+    std::cout << receivedMessage.ParseFromString(std::string(buffer)) << std::endl;
+    std::cout << "Name of client: " << receivedMessage.sender_name() << ", message from client: " << receivedMessage.sender_message();
 
     // closing the socket.
     close(serverSocket);
