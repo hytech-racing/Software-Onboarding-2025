cb8243fc41d353311ccd82c3ed4c30539ce7bd04
Kevin Xiang <kxiang644@gmail.com>
2025-01-18 18:07:27 -0500

    Lap2Part1 Full implementation with overlays

--- Kevin/Lab2/Part1/flake.nix
+++ Kevin/Lab2/Part1/flake.nix
@@ -5,8 +5,22 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     };
 
-    outputs = { self, nixpkgs} : {
-        packages.aarch64-darwin = callPackage ./default.nix {};
-    }
+    outputs = { self, nixpkgs} :
+        let
+            hello_library_overlay = final: prev: {
+                hello_lib = final.callPackage ./default.nix {};
+            };
+
+            overlays_to_install = [ hello_library_overlay ];
+
+            pkgs = import nixpkgs {
+                system = "aarch64-darwin";
+                overlays = [self.overlays.default];
+            };
+        in {
+            packages.aarch64-darwin.default = pkgs.hello_lib;
+            overlays.default = nixpkgs.lib.composeManyExtensions overlays_to_install;
+        };
+
 
 }
