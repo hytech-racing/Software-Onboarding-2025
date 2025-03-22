84cd9f81fafbfda138bd9b6474c9189ac7dc3333
acr12856 <rajagopal.amogh@gmail.com>
2025-03-22 14:43:40 -0700

    fixed includes, removed result for lab3

--- AmoghRajagopal/lab3/app/udp_server.cpp
+++ AmoghRajagopal/lab3/app/udp_server.cpp
@@ -1,5 +1,5 @@
-#include "../src/udp_socket.h"
-#include "../generated/message.pb.h"  // Include the generated Protobuf header
+#include "udp_socket.h"
+#include "message.pb.h"  // Include the generated Protobuf header
 #include <iostream>
 
 int main() {
